d4565720dbc10a4fa4b035a8c7f0c8adc9d6ee03
Alira-Coffman <aliradawn@gmail.com>
2024-01-11 19:44:00 -0600

    week 4

---
+++ 1-9-2024/script.js
@@ -0,0 +1,64 @@
+//Java & Javascript
+//recruiter -> manager -> technical -> take home -> offer or no
+
+/**Variables
+ * fhdjsafhj
+ * dfahjkdsfa
+ */
+//single line comment
+var x; //declaration
+x = 7; //initialization
+var age = 55; //assigning a value
+let name = "John";
+var age = 7;
+// let name = "doe";
+
+const pi = 3.14;
+age = 6;
+
+var add = age + pi;
+
+console.log(x);
+
+console.log(add);
+
+/**Create two variables that hold the name of John Smith; Add those together */
+let firstName = "John";
+let lastName = "Smith";
+let isItSnowing = true; //boolean
+let obj = { name: "john" };
+//string concatationation
+
+console.log(firstName + " " + lastName);
+
+console.log(age + firstName); //6John
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof pi);
+console.log(typeof isItSnowing);
+console.log(typeof obj);
+
+///OPERATIONS
+let a = 5;
+let b = 5.5;
+let c = 70;
+let y = 6;
+console.log(c - a);
+console.log(c * a);
+console.log(c / a);
+console.log(c % a);
+console.log(c % y);
+
+let z = 4;
+let t = 2;
+console.log(z % t); // 4 /2
+let k = 3;
+console.log(k % t); // 3/2
+
+console.log(k++);
+
+let l = ++k;
+console.log(l);
+console.log(--l);
+console.log(l--);
